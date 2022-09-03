d13d480be6546837ab074df5e5ce14ee027338b6
Petter Thowsen <petterthowsen@gmail.com>
2022-09-03 17:39:42 +0200

    bugfix

--- src/Pages/StructuredChildPage.php
+++ src/Pages/StructuredChildPage.php
@@ -12,24 +12,6 @@ class StructuredChildPage extends Page
     public function __construct(string $name, string $file, array $data, string $source)
     {
         parent::__construct($name, $file, $data, $source);
-        $this->fetchSiblings();
-    }
-
-    protected function fetchSiblings()
-    {
-        $f = explode('/', $this->file);
-        array_pop($f);
-
-        $pathSegments = array_slice($f, 0, count($f) - 1);
-        $parentDir = implode('/', $pathSegments) .'/children';
-        $parentName = $pathSegments[count($pathSegments) - 1];
-
-        foreach(scandir($parentDir) as $siblingFile) {
-            if ($siblingFile != '.' && $siblingFile != '..') {
-                list($name, $extension) = explode('.', $siblingFile, 2);
-                $this->siblings[] = $name;
-            }
-        }
     }
 
     public function getRoutePath():string
@@ -50,29 +32,15 @@ class StructuredChildPage extends Page
         return implode('/', array_slice($name, 0, count($name) - 2));
     }
 
+    public function getParentPage():StructuredPage
+    {
+        $parentPath = $this->getParentRoutePath();
+        return pages()->get($this->getParentRoutePath());
+    }
+
     public function renderMenu(array $classes = [], string $activeClass = 'active')
     {
-        $str = '';
-        
-        foreach($this->siblings as $name)
-        {
-            $link = url('/' .$this->getParentRoutePath() .'/' .$name);
-            
-            $a = new Element(
-                'a',
-                false,
-                [
-                    'class' => $classes,
-                    'href' => $link,
-                ],
-            );
-            
-            $a->innerHtml(ucwords(preg_replace("/[-_]/", ' ', $name)));
-            
-            $str .= $a;
-        }
-        
-        return $str;
+        return $this->getParentPage()->renderMenu($classes, $activeClass);
     }
 
 }
